e7c0e5669b26ddd47b57a2f96f2287aae8b86d2e
kaiomurz <kaiomurz@gmail.com>
2024-08-26 14:20:07 +0100

    complete functionality

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 from datetime import datetime
-import subprocess
 import time
-# sp.run(["ls -al | grep ENSE"])
+import subprocess
+import hashlib
+import sys
 
-# Run the command
-# result = subprocess.run('ls -al | grep ENSE', shell=True,) # stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-# result = subprocess.run('rclone sync ~/obdsidian/Kaiomurz/ googledrive:/DriveSyncFiles/Kaiomurz', shell=True,) # stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-while True:
-    subprocess.run('rclone sync ~/obdsidian/Kaiomurz/ googledrive:/DriveSyncFiles/Kaiomurz', shell=True,) # stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    print(datetime.now())
-    time.sleep(10)
-# Get the output
-# output = result.stdout
 
-# Print the output
-# print(output, time.time()-start)
+def load_old_hash(hashfile: str) -> str:
+    with open(hashfile, "r") as f:
+        old_hash = f.read()
+    return old_hash
 
-# rclone sync ~/obdsidian/Kaiomurz/ googledrive:/DriveSyncFiles/Kaiomurz
+
+def save_hash(hash_string: str, hashfile: str) -> None:
+    with open(hashfile, "w") as f:
+        f.write(hash_string)
+
+
+def get_new_hash(source_path: str) -> str:
+    listing = subprocess.Popen(
+        f"tree -s {source_path}", shell=True, stdout=subprocess.PIPE
+    ).stdout.read()
+    new_hash = hashlib.sha256(str(listing).encode("utf-8")).hexdigest()
+    print(str(listing))
+    return new_hash
+
+
+if __name__ == "__main__":
+    # load environment variables
+    old_hash = load_old_hash(hashfile="hashfile.txt")
+    while True:
+        new_hash = get_new_hash(source_path="~/obdsidian/Kaiomurz/")
+        print(new_hash)
+        if new_hash != old_hash:
+            subprocess.run(
+                "rclone sync ~/obdsidian/Kaiomurz/ googledrive:/DriveSyncFiles/Kaiomurz",  # replace with vars
+                shell=True,
+            )
+            print(datetime.now())
+            old_hash = new_hash
+            save_hash(old_hash, hashfile="hashfile.txt")
+        time.sleep(5)
--- trial.py
+++ trial.py
@@ -6,15 +6,8 @@ import hashlib
 def consistent_hash(s):
     return hashlib.sha256(s.encode('utf-8')).hexdigest()
 
-# test
+
 result = subprocess.Popen('ls -al', shell=True, stdout=subprocess.PIPE)
 output = str(result.stdout.read())
-print(consistent_hash(output))
 print(output)
-
-# for i in range(10):
-#     result = subprocess.run('ls -al', shell=True,)
-#     print(consistent_hash(str(result)))
-#     time.sleep(1)
-
-#sdfsdfsdfs
+print(hashlib.sha256(output.encode('utf-8')).hexdigest())
